c12a8a3024214d7b92d8f24cc097a5997d093bde
Zsofi <galambvarizsofia@gmail.com>
2020-11-19 16:13:45 +0100

    Second biggest and smallest are separeted

---
+++ secondSmallestAndBiggest/secondBiggest.js
@@ -0,0 +1,13 @@
+
+/*
+Írasd ki egy tetszőleges elemszámú,
+csak egész számokat tartalmazó tömb második legnagyobb elemét!
+*/
+
+const arr = [1, 2, 3, 4, 5];
+
+function getNthLargestElementFromAnArray(arr, nth) {
+    return bubbleSort(arr)[arr.length - nth];
+}
+
+console.log(getNthLargestElementFromAnArray(arr, 2));
